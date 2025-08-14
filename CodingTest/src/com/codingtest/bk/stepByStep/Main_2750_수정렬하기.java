3f40f3536d985014fc0f1bd473ccad718a4eec6b
soyeon-noh <1010qwerty12@gmail.com>
2025-08-14 18:02:22 +0900

    bk

--- CodingTest/src/com/codingtest/bk/stepByStep/Main_2750_수정렬하기.java
+++ CodingTest/src/com/codingtest/bk/stepByStep/Main_2750_수정렬하기.java
@@ -1,5 +1,24 @@
 package com.codingtest.bk.stepByStep;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main_2750_수정렬하기 {
 
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[] numbers = new int[N];
+		
+		for(int i = 0; i < N; i++) {
+			numbers[i] = Integer.parseInt(br.readLine());
+		}
+		
+		
+		
+		
+	}
+	
+	
 }
