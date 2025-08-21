e36852c7b78a896d4608eb7abb4f794dd0f679bf
soyeon-noh <1010qwerty12@gmail.com>
2025-08-21 13:52:50 +0900

    SWEA 1227

---
+++ CodingTest/src/com/codingtest/swea/Solution_1227_미로2.java
@@ -0,0 +1,76 @@
+package com.codingtest.swea;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution_1227_미로2 {
+	
+	static int ans;
+	static int[] startPos = new int[2];
+	static int[] endPos = new int[2];
+	static int[][] map = new int[100][100];
+	static int[] dx = {-1, 1, 0, 0 };
+	static int[] dy = {0, 0, -1, 1};
+//	static int[][] visited;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int T = 10; // 테스트 케이스 수  10개
+		
+		for(int tc = 1; tc <= T; tc++) {
+			br.readLine(); // 테케 번호 빼내기
+			
+			for(int i = 0; i < 100; i++) {
+
+				String input = br.readLine();
+//				System.out.println(input);
+				for(int j = 0; j < 100; j++) {
+					map[i][j] = Integer.parseInt(String.valueOf(input.charAt(j)));	
+					if(map[i][j] == 2) {
+						startPos[0] = i;
+						startPos[1] = j;
+					} else if(map[i][j] == 3) {
+						endPos[0] = i;
+						endPos[1] = j;
+					}
+				}
+			}// 입력끝
+			
+			
+			dfs(startPos[0], startPos[1]);
+			
+
+			sb.append("#").append(tc).append(" ").append(ans).append("\n");
+			// 초기화
+			ans = 0;
+		}
+		
+		System.out.println(sb);
+	}
+
+	private static void dfs(int cx, int cy) {
+		
+		
+		// 방문처리
+		map[cx][cy] = 1;
+		
+		for(int d = 0; d < dx.length; d++) {
+		
+			int nx = cx + dx[d];
+			int ny = cy + dy[d];
+			
+			if(map[nx][ny] == 0) {
+				dfs(nx, ny);
+			} else if(map[nx][ny] == 3) {
+				ans = 1;
+				return;
+			}
+			
+		}
+		
+	}
+}
+
---
+++ CodingTest/src/com/codingtest/swea/Solution_1238_Contact_유재영.java
@@ -0,0 +1,78 @@
+package com.codingtest.swea;
+
+import java.util.*;
+import java.io.*;
+/*
+1)summary
+	비상연락망과 연락 시작 당번에 대한 정보가 주어질 때, 
+	가장 나중에 연락을 받게 되는 사람 중 번호가 가장 큰 사람을 구하기
+
+2)strategy
+시작 정점으로부터 시작해서 단계별로 연락 가능한 모든 정점들을 "동시에" 처리하면서 진행해야 함. 	
+레벨별로 접근(BFS)
+int 타입 visit 배열에 방문한 순서를 저장함
+
+3)note
+1.중간 중간에 비어있는 번호가 있을 수 있다.
+2.연락 인원은 최대 100명이며, 부여될 수 있는 번호는 1이상, 100이하이다.
+3.연락을 전하는 속도는 일정하며 연락 가능한 사람들에게 동시에 전달된다. 
+4.동일한 {from, to}쌍이 여러 번 반복되는 경우도 있다.
+5.입력받는 데이터는 {from, to, from, to, …} 의 순서로 들어온다.
+
+  
+ * */
+public class Solution_1238_Contact_유재영 {
+	static int[][] map;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = 10;
+
+		for (int tc = 1; tc <= T; tc++) {
+			int N = sc.nextInt();// 입력데이터 총갯수(from, to)		
+			int start = sc.nextInt();//시작정점
+			
+			map = new int[101][101];//1~100
+
+			for (int i = 1; i <= N/2 ; i++) {
+				int from = sc.nextInt();
+				int to = sc.nextInt();
+				map[from][to] = 1;				
+			}
+			
+			int result = bfs(start);
+			System.out.println("#" + tc + " " + result);
+		}
+	}
+	
+	static int bfs(int start) {
+		Queue<Integer> q = new LinkedList<>();
+		int[] visited = new int[101];//방문 여부와 함께 방문 순서
+		
+		q.offer(start);
+		visited[start] = 1;		
+		
+		int from = 0;
+		while(!q.isEmpty()) {			
+			from = q.poll();
+			
+			for (int to = 0; to < 101; to++) {
+				if(map[from][to] == 1 && visited[to] == 0) {
+					q.offer(to);
+					visited[to] = visited[from] + 1;
+				}
+			}			
+		}//연결되어 있는 모든 정점들이 방문체크 끝남
+		
+		//가장 나중에 방문한 정점 찾아야 돼
+		int last = visited[from];//q에서 제일 마지막으로 꺼내진 정점의 방문 순서
+				
+		int ans = 0;
+		for (int no = 1; no < 101; no++) {
+			if(visited[no] == last) {
+				ans = ans < no ? no: ans;
+			}				
+		}		
+		return ans;		
+	}
+}
