d1d474f16d675b222353c4640598f3754d5174d3
soyeon-noh <1010qwerty12@gmail.com>
2025-08-27 17:59:13 +0900

    미생물

--- CodingTest/src/com/codingtest/swea/Solution_1251_노소연.java
+++ CodingTest/src/com/codingtest/swea/Solution_1251_노소연.java
@@ -1,10 +1,17 @@
 package com.codingtest.swea;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 // 하나로
 // https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV15StKqAQkCFAYD
 public class Solution_1251_노소연 {
 
 	public static void main(String[] args) {
 		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
 	}
 }
---
+++ CodingTest/src/com/codingtest/swea/Solution_1251_하나로.java
@@ -0,0 +1,9 @@
+package com.codingtest.swea;
+
+// 
+public class Solution_1251_하나로 {
+
+	public static void main(String[] args) {
+		
+	}
+}
