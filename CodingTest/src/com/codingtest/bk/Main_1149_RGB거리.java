2c1834089d921d79fa9576c3cd4f10ee654745d8
soyeon-noh <1010qwerty12@gmail.com>
2025-09-09 17:59:03 +0900

    RGB

--- CodingTest/src/com/codingtest/bk/Main_1149_RGB거리.java
+++ CodingTest/src/com/codingtest/bk/Main_1149_RGB거리.java
@@ -21,38 +21,47 @@ import java.util.StringTokenizer;
  * - 빨강 초록 파랑으로 칠하는 비용 x n번
  */
 public class Main_1149_RGB거리 {
-
 	
+	// 생각을 해봤는데. DP문제든 뭐든
+	// 나머지는 알바가 아니라는 태도가 필요한 것 같음
 	static int N;
 	static int R, G, B;
 	static int[][] list;
+	static int min = Integer.MAX_VALUE;
 	public static void main(String[] args) throws Exception{
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
 
 		N = Integer.parseInt(br.readLine());
-		list = new int[N][2]; // [0] 여기까지의 최소값 / [1] 0,1,2 = R,G,B
+		list = new int[N][3]; // Nx3 배열로 만들 생각을 어떻게 하는 거임?
+		// N+1로 해서 0인덱스를 만들어야 아래의 초기값을 안 적어도됨.
+		// 집을 하나도 안칠했을 때 0원이라는 0인덱스가 있으면 점화식 가능
 		
-		int min;
-		for(int n = 1; n <= N; n++) {
-			min = Integer.MAX_VALUE;
+		// 초기값 입력
+		st = new StringTokenizer(br.readLine(), " ");
+		list[0][0] = Integer.parseInt(st.nextToken()); 
+		list[0][1] = Integer.parseInt(st.nextToken()); 
+		list[0][2] = Integer.parseInt(st.nextToken()); 
+		
+		for(int n = 1; n < N; n++) {
+			st = new StringTokenizer(br.readLine(), " ");
 			
-			st = new StringTokenizer(br.readLine());
-			R = Integer.parseInt(st.nextToken());
-			G = Integer.parseInt(st.nextToken());
+			R = Integer.parseInt(st.nextToken()); 
+			G = Integer.parseInt(st.nextToken()); 
 			B = Integer.parseInt(st.nextToken());
 			
-			if(n==1) {
-				if(R > G)
-				min = Math.min(R,G);
-				min = Math.min(min, B);
-				
-			}else if(n>1){
-				
-			}
-			
+			list[n][0] = R + Math.min(list[n-1][1], list[n-1][2]);
+			list[n][1] = G + Math.min(list[n-1][0], list[n-1][2]);
+			list[n][2] = B + Math.min(list[n-1][0], list[n-1][1]);
 		}
 		
+		for(int i = 0; i < 3; i++) {
+			min = Math.min(min, list[N-1][i]);
+		}
+		
+		sb.append(min);
+		System.out.println(sb.toString());
 	}
 }
---
+++ CodingTest/src/com/codingtest/swea/Solution_3307_최장증가부분수열.java
@@ -0,0 +1,54 @@
+package com.codingtest.swea;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 최장증가 부분수열 - DP
+// 이진검색 사용 x 방법으로 풀어보았다.
+public class Solution_3307_최장증가부분수열 {
+
+	static int N;
+	static int[] list;
+	static int[] LIS; // 인덱스가 맨 마지막에 있을 때 길이값
+	public static void main(String[] args) throws Exception {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		for(int tc = 1; tc <= T; tc++) {
+			
+			N = Integer.parseInt(br.readLine());
+			list = new int[N];
+			LIS = new int[N];
+			
+			st = new StringTokenizer(br.readLine());
+			for(int n = 0; n < N; n++) {
+				list[n] = Integer.parseInt(st.nextToken());
+			}
+			
+			int max = 0;
+			for(int i = 0; i < N; i++) { // LIS[i]를 갱신하기 위해 반복을 돌려보자
+				LIS[i] = 1; // 초기화 // 일단 최소 나자신. 1로 초기화
+				for(int j = 0; j < i; j++) { // 0~ i-1 까지 돌면서 LIS[j]값을 이용해보자
+					// 만약 i번 인덱스의 값이 j번보다 크면서
+					// j for문을 돌면서 나온 LIS[i] 값보다 큰 것을 찾아내는 거임
+					if(list[i] > list[j] && LIS[i] < LIS[j] + 1) {
+						LIS[i] = LIS[j] + 1;
+					}
+				}
+				// LIS[i] 값을 구했으니 비교해서 max값 찾기
+				max = Math.max(LIS[i], max);
+			}
+			
+			
+			sb.append("#").append(tc).append(" ").append(max).append("\n");
+			
+		}
+		
+		System.out.println(sb.toString());
+	}
+}
