7b57a22e7171f1d032679996ca0a1b658d6d8ad2
soyeon-noh <1010qwerty12@gmail.com>
2025-09-17 18:00:30 +0900

    n과m

--- CodingTest/src/com/codingtest/bk/stepByStep/Main_15652_N과M4.java
+++ CodingTest/src/com/codingtest/bk/stepByStep/Main_15652_N과M4.java
@@ -1,5 +1,10 @@
 package com.codingtest.bk.stepByStep;
 
+import java.util.Scanner;
+
 public class Main_15652_N과M4 {
 
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+	}
 }
