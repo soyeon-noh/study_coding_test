2581a73b8cf659cebcdb0b718f9ae186a8610874
soyeon-noh <1010qwerty12@gmail.com>
2025-09-30 17:58:33 +0900

    이동하기

--- CodingTest/src/codetree/day7/강력한폭발.java
+++ CodingTest/src/codetree/day7/강력한폭발.java
@@ -2,4 +2,7 @@ package codetree.day7;
 
 public class 강력한폭발 {
 
+	public static void main(String[] args) {
+		
+	}
 }
---
+++ CodingTest/src/ssagorithm/bk/s/Main_11048_이동하기.java
@@ -0,0 +1,14 @@
+package ssagorithm.bk.s;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_11048_이동하기 {
+
+	// 
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+	}
+}
