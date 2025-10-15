0943e05a1d7d50a937df0f00ca6c4d9121d9c19a
soyeon-noh <1010qwerty12@gmail.com>
2025-10-16 00:20:04 +0900

    BK_13903

--- CodingTest/src/bk/silver/Main_13903_출근.java
+++ CodingTest/src/bk/silver/Main_13903_출근.java
@@ -25,11 +25,16 @@ public class Main_13903_출근 {
 	 * 
 	 * 출력
 	 * - 최소 몇 걸음 or -1 (출근불가) 
-	 * @throws IOException 
+	 * 
+	 * 후기 
+	 * - 답을 봄.
+	 * - 큐에 삽입되는 데이터를 배열로 하는 방법을 몰랐음
+	 * - 시작점이 여러개라고 bfs를 여러번 돌리면 안 됨. 그냥 큐에 넣기
+	 * - bfs는 가장 먼저 도달하는 게 최소거리므로 따로 갱신할 필요 x 
 	 */
 	
 	static int R, C, N;
-	static int max;
+	static int min = Integer.MAX_VALUE;
 	static int[][] map;
 	static boolean[][] visited;
 	static int[] dr;
@@ -49,6 +54,8 @@ public class Main_13903_출근 {
 			}
 		}
 		
+		N = Integer.parseInt(br.readLine());
+		
 		dr = new int[N];
 		dc = new int[N];
 		for(int i = 0; i < N; i++) {
@@ -58,48 +65,60 @@ public class Main_13903_출근 {
 			dc[i] = Integer.parseInt(st.nextToken());
 		}
 		// 입력끝
+	
+		// bfs를 모든 시작점마다 따로 돌려서 시간초과 발생
+//		for(int c = 1; c < C+1; c++) {
+//			if(map[1][c] == 0) continue; // 가로면 ㅂㅂ 
+//			bfs(c); // 시작 좌표 c 찾기 
+//		}
+		bfs();
 		
-		for(int c = 1; c < C+1; c++) {
-			if(map[1][c] == 0) continue; // 가로면 ㅂㅂ 
-			bfs(c); // 시작 좌표 c 찾기 
-		}
-		
-		if(max == 0) {
+		if(min == Integer.MAX_VALUE) {
 			System.out.println(-1);
 		}else {
-			System.out.println(max);
+			System.out.println(min);
 		}
 	}
 	
-	private static void bfs(int c) {
+	private static void bfs() {
 		Queue<int[]> q = new ArrayDeque<>();
-		q.add(new int[] {1,c, 0}); //배열 add할때 이렇게 해야하는지 몰라서 검색함
-		// depth도 추가해줌 
-		visited[1][c] = true;
 		
+		// 오답노트 : 이거 왜 여기 있어야 하는지 확인 
+		for(int c = 1; c < C+1; c++) {
+			if(map[1][c] == 1) {
+				q.add(new int[] {1,c, 0}); //배열 add할때 이렇게 해야하는지 몰라서 검색함
+				// depth도 추가해줌
+				visited[1][c] = true;
+			}
+		}
 		
 		while(!q.isEmpty()) {
-			int[] now = q.poll();
 			
+			int[] now = q.poll();
+
 			for(int i = 0; i < N; i++) {
+				
 				int nr = now[0] + dr[i];
 				int nc = now[1] + dc[i];
 				int nDepth = now[2] + 1;
 				
-				// 사방을 0으로 만들어서 범위체크 필요 x 
-				if(map[nr][nc] == 1 && !visited[nr][nc]) {
-					if(nr == R && max < nDepth) {
-						max = nDepth;
+				if(check(nr, nc) && map[nr][nc] == 1 && !visited[nr][nc]) {
+				
+					if(nr == R ) {
+				
+						min = nDepth;
+						return; //min 갱신 로직이 불필요하대. BFS는 가장 먼저 도착한 경로가 최소 거리 
 					}
 					q.add(new int[] {nr, nc, nDepth});
 					visited[nr][nc] = true;		
 				}
 			}
 			
-		}
-		
-		
-		
+		}	
+	}
+
+	private static boolean check(int nr, int nc) {
+		return nr > 0 && nc > 0 && nr < R+1 && nc < C+1;
 	}
 
 }
