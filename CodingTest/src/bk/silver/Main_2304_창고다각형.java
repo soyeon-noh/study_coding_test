c8b50d8bf383e4ae25909629e852bcf8bf245ebc
nohsoyeon <1010qwerty12@gmail.com>
2025-10-22 17:49:33 +0900

    BK 2304

--- CodingTest/src/bk/silver/Main_2304_창고다각형.java
+++ CodingTest/src/bk/silver/Main_2304_창고다각형.java
@@ -2,6 +2,15 @@ package bk.silver;
 
 public class Main_2304_창고다각형 {
 
+	/**
+	 * 입력
+	 * - 기둥 개수 N (1000이하)
+	 * - 기중의 왼쪽면의 위치 L, 높이를 나타내는 정수 H
+	 * 		(둘다 1000이하)
+	 * 
+	 * 출력
+	 * - 다각형의 면적 
+	 */
 	public static void main(String[] args) {
 		
 	}
