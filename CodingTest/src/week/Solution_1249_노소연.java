dfd0a0ad1b3195ff86b885966044e895c9f93e22
nohsoyeon <1010qwerty12@gmail.com>
2025-10-30 17:53:49 +0900

    SWEA 1249_보급로

--- CodingTest/src/week/Solution_1249_노소연.java
+++ CodingTest/src/week/Solution_1249_노소연.java
@@ -1,9 +1,11 @@
-package week;
+package week; // 보급로
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
@@ -34,69 +36,87 @@ import java.util.StringTokenizer;
  * - dx dy
  * 
  */
-public class Solution_1249_보급로 {
+public class Solution_1249_노소연 {
 
 	static int T, N;
-	static int ans;
-	static int[] dr = {1, -1, 0, 0};
+	static int[] dr = {-1, 1, 0, 0};
 	static int[] dc = {0, 0, -1, 1};
 	static int[][] map;
-	static boolean[][] visited;
+	static int[][] dist;
 	static StringBuilder sb = new StringBuilder();
-	
-	static class Node {
+	// 웅니 화이팅~~
+	static class Node implements Comparable<Node>{
 		int r;
 		int c;
-		int dis;
-		public Node(int r, int c, int dis) {
+		int cost;
+		
+		public Node(int r, int c, int cost) {
 			super();
 			this.r = r;
 			this.c = c;
-			this.dis = dis;
+			this.cost = cost;
+		}
+		@Override
+		public int compareTo(Node o) {
+			return Integer.compare(this.cost, o.cost);
 		}
 	}
+	
 	public static void main(String[] args) throws Exception {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
 		
 		T = Integer.parseInt(br.readLine());
 		
 		for(int tc = 1; tc <= T; tc++) {
 			N = Integer.parseInt(br.readLine());
 			map = new int[N][N];
-			visited = new boolean[N][N]; 
+			dist = new int[N][N];
 			
 			for(int r = 0; r < N; r++) {
-				st = new StringTokenizer(br.readLine());
+				String input = br.readLine();
 				for(int c = 0; c < N; c++) {
-					map[r][c] = Integer.parseInt(st.nextToken());
+					map[r][c] = input.charAt(c) - '0';
 				}
 			} // 입력완료
 			
-			ans = 0; //초기화 
-			bfs();
+			for(int[] d : dist) {
+				Arrays.fill(d, Integer.MAX_VALUE); // 무한대로 초기화
+			}
 			
-			sb.append("#").append(tc).append(" ").append(ans);
+			 
+			dijkstra();
+			
+			sb.append("#").append(tc).append(" ").append(dist[N-1][N-1]).append("\n");
+			
 		}
+		System.out.println(sb);
 		
 	}
-	private static void bfs() {
+	private static void dijkstra() {
+		PriorityQueue<Node> pq = new PriorityQueue<>();
+		dist[0][0] = 0;
+		pq.offer(new Node(0,0,0));
 		
-		Queue<Node> q = new ArrayDeque<>();
-		q.add(new Node(0,0,0));
-		visited[0][0] = true; // 방문처리 
-		
-		while(q.isEmpty()) {
-			int r = q.poll().r;
-			int c = q.poll().c;
+		while(!pq.isEmpty()) {
+			Node now = pq.poll();
+			int r = now.r;
+			int c = now.c;
+			int cost = now.cost;
+			
+			if(cost > dist[r][c]) continue; // visited 처리 대신 이걸 사용
 			
 			for(int d = 0; d < 4; d++) {
 				int nr = r + dr[d];
 				int nc = c + dc[d];
 				
 				if(nr >= 0 && nc >= 0 && nr < N && nc < N) {
-					q.add(new Node(nr, nc, map[nr][nc]));
+					
+					if(dist[nr][nc] > cost + map[nr][nc]) {
+						dist[nr][nc] = cost + map[nr][nc];
+						pq.offer(new Node(nr, nc, dist[nr][nc]));	
+					}
+					
 				}
 			}
 		}
