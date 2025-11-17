9c519ece144ed1c0cc32672bc5a3aaaa68f19fbd
노소연 <noso42@daum.net>
2025-11-17 17:57:30 +0900

    ss

---
+++ CodingTest/src/week/Main_1759_암호만들기.java
@@ -0,0 +1,5 @@
+package week;
+
+public class Main_1759_암호만들기 {
+
+}
---
+++ CodingTest/src/week/Main_2003_수들의합2.java
@@ -0,0 +1,13 @@
+package week;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_2003_수들의합2 {
+
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+	}
+}
