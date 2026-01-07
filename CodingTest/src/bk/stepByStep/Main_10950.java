8f4f9e8d1944d846b2a271d746696634157b1192
soyeon-noh <1010qwerty12@gmail.com>
2026-01-07 23:56:52 +0900

    BK_10950

--- CodingTest/src/bk/stepByStep/Main_10950.java
+++ CodingTest/src/bk/stepByStep/Main_10950.java
@@ -1,11 +1,11 @@
-package week;
+package bk.stepByStep;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Main_10950 {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -28,3 +28,4 @@ public class Main {
 		
 	}
 }
+
