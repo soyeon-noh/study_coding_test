f83f1adfc99cc88f7f715963acf146c261d5b3b6
soyeon-noh <1010qwerty12@gmail.com>
2026-01-15 23:58:45 +0900

    bk_3407_실버1

---
+++ CodingTest/src/week/_Main_3407_맹세.java
@@ -0,0 +1,34 @@
+package week; //실버 1
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/*
+ * 문제 이해
+ * - 단어가 주어졌을 때 원소 기호로 이루어진 단어로 이루어져있는지 구함
+ *
+ * 입력
+ * - 테스트 케이스의 개수 T
+ * - 다음 T개의 줄에는 알파벳 소문자로만 된 단어가 하나
+ * - 단어의 길이는 50,000을 넘지 않으며 양수
+ *
+ * 출력
+ * - YES
+ * - NO
+ */
+public class _Main_3407_맹세 {
+    static int T;
+    static String[] arr;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        T = Integer.parseInt(br.readLine());
+
+        for(int tc = 1; tc <= T; tc++){
+
+            String input = br.readLine();
+        }
+
+    }
+}
